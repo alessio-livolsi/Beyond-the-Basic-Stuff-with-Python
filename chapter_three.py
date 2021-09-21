f015e79497a3ed471fe0352975f4b46d0e76b5e3
Alessio <alessio.livolsi.27@gmail.com>
2021-09-21 12:24:37 +0100

    Finished Chapter three  21/09/21

---
+++ chapter_three.py
@@ -0,0 +1,11 @@
+print("Hello there, friend!\nHow are you?")
+
+print("Hello\tthere,\tfriend!\nHow\tare\you?")
+
+
+# Code formatting Black example
+def getCatAmount():
+    numCats = input("How many cats do you have?")
+
+    if int(numCats) < 6:
+        print("You should get more cats.")
