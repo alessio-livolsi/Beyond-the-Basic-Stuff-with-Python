fd951a6269036cd3b4e74a739f0c0645daba2999
Alessio <alessio.livolsi.27@gmail.com>
2021-09-21 11:30:14 +0100

    Finished Chapter two 21/09/21

--- chapter_two.py
+++ chapter_two.py
@@ -1,6 +1,13 @@
 # Code ran in shell
-from pathlib import Path
-import os
-
+# from pathlib import Path
 # path("spam") / "bacon" / "eggs"
-print(Path.cwd())
+
+# Code ran in shell
+# import subprocess, locale
+# procObj = subprocess.run(['ls', '-al'], stdout=subprocess.PIPE)
+# outputStr = procObj.stdout.decode(locale.getdefaultlocale()[1])
+# print(outputStr)
+
+# from pathlib import Path
+# import os
+# print(Path.cwd())
